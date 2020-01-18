4d78c851edfee0e651a5005a5a0ba9de49e970b7
WhiteOwl314 <parksj914@naver.com>
2020-01-19 01:12:30 +0900

    Create PersonRepository

--- src/main/java/sideproject1/contact/repository/PersonRepository.java
+++ src/main/java/sideproject1/contact/repository/PersonRepository.java
@@ -1,4 +1,7 @@
 package sideproject1.contact.repository;
 
-public class PersonRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import sideproject1.contact.domain.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
